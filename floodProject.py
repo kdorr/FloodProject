0613e55e20c896d415446de27a14072b1e19304c
tturner2 <timothy.turner@valpo.edu>
2017-02-26 22:11:46 -0600

    Fixed input of data and added a test csv of limited foreclosure data
    ^^

--- floodProject.py
+++ floodProject.py
@@ -3,12 +3,22 @@ Created on Thu Feb 23 20:27:36 2017
 
 @author: Tim
 """
-
+import urllib
 import numpy
 import csv
 import sys
-
-foreclosures = open("C:\\Users\\Tim\\Desktop\\Programming\\DataScience\\FloodProjectData\\County_HomesSoldAsForeclosures-Ratio_AllHomes.csv","r")
-print (foreclosures.read())
+import os
+#foreclosureDataURL = "https://raw.github.com/kdorr/FloodProject/blob/master/County_HomesSoldAsForeclosures-Ratio_AllHomes-2.csv"
+#response = urllib.request.urlopen(foreclosureDataURL)
+#print (os.listdir())
+#foreclosures = csv.reader(df)
+foreclosuresArray = []
+#foreclosures = open('County_HomesSoldAsForeclosures-Ratio_AllHomes-2.csv',"r")
+#for line in foreclosures:
+#    foreclosuresArray.append(line.strip().split(','))
+foreclosures = open('TestForeclosures.csv',"r")
+for line in foreclosures:
+    foreclosuresArray.append(line.strip().split(','))
+print(foreclosuresArray)
 
     
