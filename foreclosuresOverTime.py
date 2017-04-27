6b365e356df911b90e09d049983f89dcd4432289
kdorr <kimberly.orr97@gmail.com>
2017-04-26 21:28:10 -0500

    Adding Script and CSV
    Add script that transforms our foreclosure data and the resulting data.

--- foreclosuresOverTime.py
+++ foreclosuresOverTime.py
@@ -2,17 +2,16 @@ import pandas as pd
 from bokeh.plotting import figure, output_file, show
 
 #Read flood data
-floodFile ="C:/Users/Kimberly/Documents/GitHub/FloodProject/dallasFloods.csv"
+floodFile ="dallasFloods.csv"
 dateparse = lambda x: pd.datetime.strptime(x, '%m/%d/%Y')
 floods = pd.read_csv(floodFile, parse_dates=['BEGIN_DATE'], date_parser=dateparse)
 
 #clean flood data
 cleanFloods = floods[['CZ_NAME_STR', 'BEGIN_DATE', 'EVENT_TYPE', 'DAMAGE_PROPERTY_NUM']]
 cleanFloods = cleanFloods.drop_duplicates()
-#print(cleanFloods.head(n=7))
 
 #read foreclosure data
-foreclosureFile = "C:/Users/Kimberly/Documents/ForeclosuresTransposed.csv"
+foreclosureFile = "ForeclosuresTransposed.csv"
 dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m')
 foreclosures = pd.read_csv(foreclosureFile, parse_dates=['Date'], date_parser=dateparse)
 
@@ -31,9 +30,8 @@ for i in range(len(cleanFloods)):
         if(date.month == foreclosures.Date.iloc[j].month and date.year == foreclosures.Date.iloc[j].year):
             fcl = foreclosures.Dallas.iloc[j]
     y=[fcl-1,fcl+1]
-    #p.line(x,y, color='green')
     p.circle(x,fcl, color='blue')
 
 #output file and show
-output_file("C:/Users/Kimberly/Documents/bokeh2.html")
+output_file("bokeh2.html")
 show(p)
