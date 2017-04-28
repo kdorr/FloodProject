3e2979611aad64f58f5da25b647131d83f186130
kdorr <kimberly.orr97@gmail.com>
2017-04-28 17:29:23 -0500

    Functional Normalized Plot
    For each flood, plots the change in foreclosures from the initial number
    of foreclosures over a specified amount of time.

--- normalizedPlot.py
+++ normalizedPlot.py
@@ -3,8 +3,36 @@
 
 import foreclosuresOverTime as data
 import pandas as pd
+from dateutil.relativedelta import *
+import datetime
 from bokeh.plotting import figure, output_file, show
 
 fcl = data.getForeclosures()
-flood = data.getFloods()
+floods = data.getFloods()
 
+#create plot
+p = figure(x_axis_label='dates', y_axis_label='number of foreclosures')
+
+numMonths = 18
+
+#for each flood, plot number of foreclosures - number of foreclosures during initial month
+for i in range(len(floods)):
+    date = floods.BEGIN_DATE.iloc[i]
+    xs = []
+    for q in range(numMonths):
+        xs.append(q)
+    ys = []
+    #x=[date,date]
+    for k in range(numMonths):
+        x = date + relativedelta(months=+k)
+        #print(x)
+        #locate the y-value of the foreclosures at the time of the flood
+        for j in range(len(fcl)):
+            if(x.month == fcl.Date.iloc[j].month and x.year == fcl.Date.iloc[j].year):
+                y = fcl.Dallas.iloc[j]-fcl.Dallas.iloc[j-k]
+        ys.append(y)
+
+    p.line(xs,ys)
+
+output_file("normalizedPlot.html")
+show(p)
