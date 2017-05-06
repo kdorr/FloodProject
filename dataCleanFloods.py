97ad296361e887dfcb8994239f2bca0e9d0e88c9
kdorr <kimberly.orr97@gmail.com>
2017-05-05 20:55:29 -0500

    Cleaned Up dataCleanFloods.py
    Added comments.

--- dataCleanFloods.py
+++ dataCleanFloods.py
@@ -5,33 +5,25 @@ floodFile ="dallasFloods.csv"
 dateparse = lambda x: pd.datetime.strptime(x, '%m/%d/%Y')
 floods = pd.read_csv(floodFile, parse_dates=['BEGIN_DATE'], date_parser=dateparse)
 
-#clean flood data
+#Reduce number of columns in floods
 floods = floods[['CZ_NAME_STR', 'BEGIN_DATE', 'EVENT_TYPE', 'DAMAGE_PROPERTY_NUM']]
-#floods = floods.drop_duplicates()
-print(floods)
 
+#Remove duplicates (only report one flood per month)
+#remove floods that are after May 2015. Any dates after May 2015 cause issues in
+#other scripts because we run out of foreclosure data to compare to.
 currentMonth = 0
 currentYear = 0
-print("in process")
-print(floods.axes)
-#floods.drop(11, axis=0, inplace=True)
-floodsLength = len(floods)
-print("floodsLength")
-print(floodsLength)
-print("floods.index")
-print(len(floods.index))
 cleanedFloods = pd.DataFrame(columns=floods.columns)
-for i in range(floodsLength):
+for i in range(len(floods)):
     previousMonth = currentMonth
     previousYear = currentYear
     currentMonth = floods.BEGIN_DATE.iloc[i].month
     currentYear = floods.BEGIN_DATE.iloc[i].year
+    #Append a row if to a new dataframe if the current date is not a duplicate and
+    #the current date is not after May 2015.
     if((currentMonth != previousMonth or currentYear != previousYear) and (currentYear < 2016)):
         if(currentYear == 2015 and currentMonth > 5):
             pass
         else:
             row = floods.iloc[i]
             cleanedFloods = cleanedFloods.append(row, ignore_index=True)
-
-print("Cleaned:")
-print(cleanedFloods)
